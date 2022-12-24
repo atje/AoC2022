127f0b4d3e7712085b0d2fc0cbf7cf26d41d276a
Andreas Tjernsten <andreas@tjernsten.se>
2022-12-24 08:00:56 +0100

    Updated to solve second part of day 1 challenge

--- aoc1.go
+++ aoc1.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 )
 
+// Read all lines from a file
 func readLines(path string) ([]string, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -23,7 +25,33 @@ func readLines(path string) ([]string, error) {
 	return lines, scanner.Err()
 }
 
+// Return list of hashmap keys sorted by value in descending order
+func sortByCalories(hm map[int]int) []int {
+	keys := make([]int, 0, len(hm))
+
+	for key := range hm {
+		keys = append(keys, key)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return hm[keys[i]] > hm[keys[j]]
+	})
+	return keys
+}
+
+func sumCalories(keys []int, hm map[int]int, len int) int {
+	sum := 0
+	for i := 0; i < len; i++ {
+		sum += hm[keys[i]]
+		fmt.Println("value", hm[keys[i]], "sum", sum)
+	}
+	return sum
+}
+
 func main() {
+	const sumElfs = 3
+
+	m := make(map[int]int)
 
 	if len(os.Args) < 2 {
 		fmt.Println("Missing parameter, provide file name!")
@@ -45,12 +73,18 @@ func main() {
 				max_calories = calories
 				max_elf = elf_count
 			}
-			calories = 0
+			m[elf_count] = calories
 			elf_count++
+			calories = 0
+
 			fmt.Println("line", i, "\nCounting new Elf", elf_count)
 		}
 		cal, _ := strconv.Atoi(line)
 		calories += cal
 	}
 	fmt.Println("Elf #", max_elf, "calories", max_calories)
+
+	cal_count := sumCalories(sortByCalories(m), m, sumElfs)
+	fmt.Println("Calorie sum for top", sumElfs, "Elfs:", cal_count)
+
 }
