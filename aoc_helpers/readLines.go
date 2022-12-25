605ff1fad8de55b07b5028360c975273229fcbae
Andreas Tjernsten <andreas@tjernsten.se>
2022-12-25 11:30:47 +0100

    Added helper package

---
+++ aoc_helpers/readLines.go
@@ -0,0 +1,24 @@
+package aoc_helpers
+
+import (
+	"bufio"
+	"os"
+)
+
+// Read all lines from a file into an array of strings
+func ReadLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func main() {}
