ee161fb8432878cd24ca52f431a91256fc4c212e
Peter Kerwien <peter.kerwien@gmail.com>
2023-07-31 09:00:40 +0200

    Rename test input

--- day01/aoc1_test.go
+++ day01/aoc1_test.go
@@ -5,14 +5,14 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	calories := readFile("test_input.txt")
+	calories := readFile("input_test.txt")
 	if part1(calories) != 24000 {
 		t.Fail()
 	}
 }
 
 func TestPart2(t *testing.T) {
-	calories := readFile("test_input.txt")
+	calories := readFile("input_test.txt")
 	if part2(calories) != 45000 {
 		t.Fail()
 	}
