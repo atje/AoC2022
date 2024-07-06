035ef16e5f5b6460c7c45e5b2bafdfcb8155b33f
Peter Kerwien <peter@kerwien.se>
2024-07-06 12:04:12 +0200

    Add MergeSort() test
    Add simple tests of the MergeSort() function.

--- aoc_helpers/mergeSort_test.go
+++ aoc_helpers/mergeSort_test.go
@@ -1,14 +1,28 @@
-package main
+package aoc_helpers
 
 import (
-	"AoC2022/aoc_helpers"
 	"testing"
 )
 
-var mergeSortTests = []aoc_helpers.File_resT{
-	{Fname: "sort_test_1.txt", Res: 13},
-}
-
 func TestMergeSort(t *testing.T) {
-	aoc_helpers.ExecTests(MergeSort, MergeSortTests)
+	// Test ascending order.
+	fn := func(left int, right int) bool {
+		return left < right
+	}
+	sorted_slice := MergeSort([]int{38, 27, 43, 10}, fn)
+	sorted := [4]int{}
+	copy(sorted[:], sorted_slice)
+	if sorted != [4]int{10, 27, 38, 43} {
+		t.Fatalf("Sort failed")
+	}
+	// Test descending order.
+	fn = func(left int, right int) bool {
+		return left > right
+	}
+	desc_sorted_slice := MergeSort([]int{11, 23, 17, 3, 5}, fn)
+	desc_sorted := [5]int{}
+	copy(desc_sorted[:], desc_sorted_slice)
+	if desc_sorted != [5]int{23, 17, 11, 5, 3} {
+		t.Fatalf("Sort failed")
+	}
 }
