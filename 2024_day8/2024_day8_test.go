ac9647a776dd0ab6ea5b3d70ddbd489bdb9a501d
Andreas Tjernsten <35493622+atje@users.noreply.github.com>
2025-03-30 19:45:51 +0000

    2024 day8, part 2 solution

--- 2024_day8/2024_day8_test.go
+++ 2024_day8/2024_day8_test.go
@@ -5,11 +5,23 @@ import (
 	"testing"
 )
 
-var p1tests = []aoc_helpers.File_resT{
-	{Args: []string{"example.txt"}, Res: 14},
-	{Args: []string{"input.txt"}, Res: 367},
+func TestSolvePart1(t *testing.T) {
+	tests := []aoc_helpers.File_resT{
+		{Args: []string{"debug.txt"}, Res: 2},
+		{Args: []string{"debug2.txt"}, Res: 4},
+		{Args: []string{"example.txt"}, Res: 14},
+		{Args: []string{"input.txt"}, Res: 367},
+	}
+
+	aoc_helpers.ExecTests(t, solvePart1, tests)
 }
 
-func TestSolvePart1(t *testing.T) {
-	aoc_helpers.ExecTests(t, solvePart1, p1tests)
+func TestSolvePart2(t *testing.T) {
+	tests := []aoc_helpers.File_resT{
+		{Args: []string{"debug3.txt"}, Res: 9},
+		{Args: []string{"example.txt"}, Res: 34},
+		{Args: []string{"input.txt"}, Res: 1285},
+	}
+
+	aoc_helpers.ExecTests(t, solvePart2, tests)
 }
