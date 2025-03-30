c64f53e0538f78b200f0cea670d1452a5d87050a
Andreas Tjernsten <35493622+atje@users.noreply.github.com>
2025-03-30 19:46:57 +0000

    2024 Day 8, part 2 solution - added missing files

--- 2024_day8/2024_day8.go
+++ 2024_day8/2024_day8.go
@@ -71,6 +71,34 @@ func addAntiNodes(m [][]byte, antennas []coordT) [][]byte {
 	return m
 }
 
+func addAntiNodes2(m [][]byte, antennas []coordT) [][]byte {
+	for i := range antennas {
+		for j := range antennas {
+			if j <= i {
+				// Skip the same antenna
+				continue
+			}
+
+			// Calculate the distance between the antennas
+			dx := antennas[j].x - antennas[i].x
+			dy := antennas[j].y - antennas[i].y
+
+			k := 0
+			for {
+				// Add the antinode to the map
+				addAntenna(m, coordT{x: antennas[i].x + k*dx, y: antennas[i].y + k*dy})
+				addAntenna(m, coordT{x: antennas[i].x - k*dx, y: antennas[i].y - k*dy})
+				k++
+				if abs(k*dx) > len(m[0]) || abs(k*dy) > len(m) {
+					break
+				}
+			}
+		}
+	}
+
+	return m
+}
+
 func printMap(m [][]byte) {
 	for i := range m {
 		for j := range m[i] {
@@ -141,8 +169,56 @@ func solvePart1(args []string) int {
 }
 
 func solvePart2(args []string) int {
-	//	fn := args[0]
+	fn := args[0]
 	res := 0
+
+	// Parse input file
+	lines, err := aoc_helpers.ReadLinesToByteSlice(fn)
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	// Find antennas, add them to the hashmap containing the antennas
+	pairs := map[byte][]coordT{}
+
+	for i := range lines {
+		for j := range lines[i] {
+			if lines[i][j] != DOT {
+				// Add antenna to the map
+				pairs[lines[i][j]] = append(pairs[lines[i][j]], coordT{x: j, y: i})
+				if *dbgFlag {
+					log.Debugf("[DEBUG] Found antenna %c at (%d,%d)", lines[i][j], j, i)
+				}
+			}
+		}
+
+	}
+
+	if *dbgFlag {
+		log.Debugf("[DEBUG] Antenna pairs: %v", pairs)
+	}
+
+	// Go through all found antennas and add antinodes to the map
+	m := initializeMap(len(lines), len(lines[0]))
+
+	for _, v := range pairs {
+		m = addAntiNodes2(m, v)
+	}
+
+	// Print the map
+	if *dbgFlag {
+		log.Debugf("[DEBUG] Antenna map:")
+		printMap(m)
+	}
+
+	// Count the number of unique antinode locations
+	for i := range m {
+		for j := range m[i] {
+			if m[i][j] == ANTINODE {
+				res++
+			}
+		}
+	}
 
 	return res
 }
